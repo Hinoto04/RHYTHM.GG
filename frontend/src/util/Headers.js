b57dc75fc9df3e44d20949b899b7ce3934a6f7d7
Hinoto04 <61084127+Hinoto04@users.noreply.github.com>
2023-08-03 18:20:30 +0900

    헤더 수정

--- frontend/src/util/Headers.js
+++ frontend/src/util/Headers.js
@@ -5,9 +5,21 @@ import "./Headers.css"
 function Headers() {
     return (
         <div class="header d-flex">
-            <div class="햄버거버튼상자"></div>
-            <div class="로고상자"></div>
-            <div class="로그인상자"></div>
+            <div class="햄버거버튼상자">
+                <button>
+                    <img class="햄버거버튼이미지" src="img/hamburger.png" alt="메뉴" />
+                </button>
+            </div>
+            <div class="로고상자">
+                <a href="/home">
+                    <img class="로고이미지" src="img/logo.png" alt="로고" />
+                </a>
+            </div>
+            <div class="로그인상자">
+                <a href="/">
+                    로그인
+                </a>
+            </div>
         </div>
     );
 }
